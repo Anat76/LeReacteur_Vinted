7ab96c19e636ee5c613a3a5e3a5975a0e3fc8d2a
Anat Plt <108489401+Anat76@users.noreply.github.com>
2023-04-15 12:12:45 +0200

    Offers Filter
    Finish exercice J6

--- routes/offer.js
+++ routes/offer.js
@@ -66,6 +66,8 @@ router.get("/offers", async (req, res) => {
     // Je destruture ce que je peux recevoir en query
     const { title, priceMin, priceMax, sort, page } = req.query;
     // console.log(req.query);
+
+    // filter
     const filter = {};
     if (title) {
       filter.product_name = new RegExp(title, "i");
@@ -77,15 +79,65 @@ router.get("/offers", async (req, res) => {
     } else if (priceMax) {
       filter.product_price = { $lte: priceMax };
     }
-    console.log(filter);
+    // console.log(filter);
+
+    // Sort
+    const sortFilter = {};
+
+    if (sort === "price-asc") {
+      // console.log("je suis dans l'ordre croissant");
+      sortFilter.product_price = 1;
+    }
+    if (sort === "price-desc") {
+      // console.log("je suis dans l'ordre décroissant");
+      sortFilter.product_price = -1;
+    }
+    // console.log(sortFilter);
+
+    // skip
+    const limit = 5;
+
+    let pageFilter = 1;
+
+    if (page) {
+      pageFilter = page;
+    }
+    const skip = (pageFilter - 1) * limit;
+    // console.log(pageFilter);
 
     //je vais chercher en BDD avec les parametres query
-    const filterOffer = await Offer.find(filter);
+    const filterOffer = await Offer.find(filter)
+      .sort(sortFilter)
+      .skip(skip)
+      .limit(limit)
+      .populate("owner", "account");
+    // .select("product_name product_price");
     // console.log(filterOffer);
-    res.status(200).json(filterOffer);
+
+    const count = await Offer.countDocuments(filter);
+
+    res.status(200).json({ count: count, offer: filterOffer });
   } catch (error) {
     res.status(500).json({ message: error });
   }
 });
-// j'exporte publish.js
+
+// route pour recherhcer par ID
+router.get("/offer/:id", async (req, res) => {
+  try {
+    // console.log(req.params);
+    // je vais chercher en BDD
+    const findOfferById = await Offer.findById(req.params.id).populate(
+      "owner",
+      "account"
+    );
+    // console.log(findOfferById);
+    // je repond au client
+    res.status(200).json(findOfferById);
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+// j'exporte offer.js
 module.exports = router;
